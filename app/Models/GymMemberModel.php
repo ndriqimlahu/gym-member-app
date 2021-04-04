1d215d59c29bd2603e0b2bb153a28fa909674ff2
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-04-04 19:33:03 +0200

    Added the getter for attribute data in Gym Member Model.

--- app/Models/GymMemberModel.php
+++ app/Models/GymMemberModel.php
@@ -1,10 +1,37 @@
 <?php
 
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class GymMemberModel
+class GymMemberModel extends Model
 {
+    use HasFactory;
 
+    public $table = 'gym_members';
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getFirstName() {
+        return $this->first_name;
+    }
+
+    public function getLastName() {
+        return $this->last_name;
+    }
+
+    public function getBirthDate() {
+        return $this->birth_date;
+    }
+
+    public function getExpireDate() {
+        return $this->expire_date;
+    }
+
+    public function getProfilePicture() {
+        return $this->profile_picture;
+    }
 }
