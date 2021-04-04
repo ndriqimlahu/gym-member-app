96b3c2e5e9f6b7d234612f98732bc2162067ae9a
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-04-04 20:09:42 +0200

    Added the function storeGymMember in Gym Member Controller.

--- app/Http/Controllers/GymMemberController.php
+++ app/Http/Controllers/GymMemberController.php
@@ -1,10 +1,19 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
+use App\Models\GymMemberModel;
+use Illuminate\Http\Request;
 
 class GymMemberController extends Controller
 {
-
+    public function storeGymMember(Request $request) {
+        $gym_members = new GymMemberModel();
+        $gym_members->first_name = $request->first_name;
+        $gym_members->last_name = $request->last_name;
+        $gym_members->birth_date = $request->birth_date;
+        $gym_members->expire_date = $request->expire_date;
+        $gym_members->profile_picture = $request->profile_picture;
+        $gym_members->save();
+    }
 }
