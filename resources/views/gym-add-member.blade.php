6f70ea9612c672bc313118bfd4bead9a783ee3fc
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-04-04 20:21:12 +0200

    Created the form data for view Gym Add Member.

--- resources/views/gym-add-member.blade.php
+++ resources/views/gym-add-member.blade.php
@@ -3,6 +3,26 @@
         <title>Gym Add Member</title>
     </head>
     <body>
-        // code
+        <form method="post" action="{{route('addedMember')}}" enctype="multipart/form-data">
+            @csrf
+            <button>View Members</button>
+            <br><br>
+            <label>First Name</label>
+            <input type="text" name="first_name">
+            <br><br>
+            <label>Last Name</label>
+            <input type="text" name="last_name">
+            <br><br>
+            <label>Birthdate</label>
+            <input type="date" name="birth_date">
+            <br><br>
+            <label>Expire Date</label>
+            <input type="date" name="expire_date">
+            <br><br>
+            <label>Profile Picture</label>
+            <input type="file" name="profile_picture">
+            <br><br>
+            <input type="submit" name="submit">
+        </form>
     </body>
 </html>
