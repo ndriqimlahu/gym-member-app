0d98de84f2d4b49e84b2438f0c8356af7148e9cc
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-04-06 00:07:12 +0200

    Added bootstrap and created the component rowtable with slot for values, and anchor with route addMember.

--- resources/views/gym-view-member.blade.php
+++ resources/views/gym-view-member.blade.php
@@ -1,8 +1,23 @@
 <html>
     <head>
         <title>Gym View Member</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     </head>
     <body>
-        // code
+        <a href="{{route('addMember')}}">Add Member</a>
+        <br><br>
+            <?php $viewGymMember = new App\Models\GymMemberModel();
+                $gymMember = $viewGymMember::all(); ?>
+
+            @foreach($gymMember as $gymMembers)
+                @component('components.rowtable')
+                    @slot('id',$gymMembers->getId())
+                    @slot('first_name',$gymMembers->getFirstName())
+                    @slot('last_name',$gymMembers->getLastName())
+                    @slot('birth_date',$gymMembers->getBirthDate())
+                    @slot('expire_date',$gymMembers->getExpireDate())
+                @endcomponent
+            @endforeach
+
     </body>
 </html>
