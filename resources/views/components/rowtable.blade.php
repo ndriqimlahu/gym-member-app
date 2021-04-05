0a47b1349359ff8176f8a7150ed2afd25cac3d86
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-04-06 00:38:56 +0200

    Added the component rowtable and using table with bootstrap classes, also retrieving values.

---
+++ resources/views/components/rowtable.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row card justify-content-center">
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>{{$id}}<br>{{$first_name}}<br>{{$last_name}}<br>{{$birth_date}}<br>{{$expire_date}} <br> {{$delete_member}}</td>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
