5b505d16c91a23cd8c7ad5b9f02f441cbe682e82
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-04-06 14:27:25 +0200

    Fixed button Add Member with bootstrap and Added new slot for edit_member with button included.

--- resources/views/gym-view-member.blade.php
+++ resources/views/gym-view-member.blade.php
@@ -4,8 +4,11 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     </head>
     <body>
-        <a class="btn btn-primary" href="{{route('addMember')}}">Add Member</a>
-        <br><br>
+        <div class="text-center">
+            <br>
+            <a class="btn btn-primary btn-lg" href="{{route('addMember')}}">Add Member</a>
+        </div>
+        <br>
             <?php $viewGymMember = new App\Models\GymMemberModel();
                 $gymMember = $viewGymMember::all(); ?>
 
@@ -16,16 +19,19 @@
                     @slot('last_name',$gymMembers->getLastName())
                     @slot('birth_date',$gymMembers->getBirthDate())
                     @slot('expire_date',$gymMembers->getExpireDate())
+                    @slot('edit_member')
+                        <button class="btn btn-success">EDIT</button>
+                    @endslot
                     @slot('delete_member')
                         <form onsubmit="return confirm('Are your sure to delete this member?');"
                         method="post" action="{{route('deleteGymMember',$gymMembers->getId())}}">
                             @method('DELETE')
                             @csrf
                             <button class="btn btn-danger">DELETE</button>
-                            </form>
+                        </form>
                     @endslot
                 @endcomponent
             @endforeach
-
+            <br><br>
     </body>
 </html>
