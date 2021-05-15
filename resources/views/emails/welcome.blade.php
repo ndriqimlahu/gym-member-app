8d17a4286e8867cac1a2003f31630d65f475031d
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-05-15 22:58:06 +0200

    Added the view welcome for emails

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to our Platform!</title>
+    </head>
+    <body>
+        <h2>Hello!</h2>
+        <h3>We are grateful to you for choosing our platform.</h3>
+        <h3>Please don't forget to visit us for more features.</h3>
+        <h2>Bye!</h2>
+    </body>
+</html>
