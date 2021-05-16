b3ce1563660abff05e5f2ca723371c6cc401d0a2
Ndriçim Lahu <ndriqim.lahu@student.cacttus.education>
2021-05-16 13:34:38 +0200

    Fixed the structure of code in view deleted for emails

--- resources/views/emails/deleted.blade.php
+++ resources/views/emails/deleted.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>The Membership has Cancelled!</title>
-</head>
-<body>
-<h2>Hello!</h2>
-<h3>We're sorry to hear that, we're also sorry that you are no longer part of our platform.</h3>
-<h3>We have to notice you because your membership is canceled.</h3>
-<h3>So please take some of our offers or subscribe again in order to enjoy our services.</h3>
-<h2>Bye!</h2>
-</body>
+    <head>
+        <title>The Membership has Cancelled!</title>
+    </head>
+    <body>
+        <h2>Hello!</h2>
+        <h3>We're sorry to hear that, we're also sorry that you are no longer part of our platform.</h3>
+        <h3>We have to notice you because your membership is canceled.</h3>
+        <h3>So please take some of our offers or subscribe again in order to enjoy our services.</h3>
+        <h2>Bye!</h2>
+    </body>
 </html>
