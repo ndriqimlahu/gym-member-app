5b8148c7fbd77e1cae4d4eaf33f7c1d861d6ed36
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2021-06-20 17:58:57 +0200

    Added the view edited for emails

---
+++ resources/views/emails/edited.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>The Membership has Updated!</title>
+    </head>
+    <body>
+        <h2>Hello!</h2>
+        <h3>We're notify you because the membership status has updated for using our platform.</h3>
+        <h3>We have to notice you because your membership is updated.</h3>
+        <h2>Bye!</h2>
+    </body>
+</html>
