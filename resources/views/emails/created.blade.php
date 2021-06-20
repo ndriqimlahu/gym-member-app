8fe2ee13dbc5608d448fa398d1616cfb81f9d5dc
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2021-06-20 17:48:50 +0200

    Added the view created for emails

---
+++ resources/views/emails/created.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>The Membership has Created!</title>
+    </head>
+    <body>
+        <h2>Hello!</h2>
+        <h3>We are grateful to you for choosing our platform for membership.</h3>
+        <h3>So, please don't forget to visit us for more features and offers.</h3>
+        <h2>Bye!</h2>
+    </body>
+</html>
