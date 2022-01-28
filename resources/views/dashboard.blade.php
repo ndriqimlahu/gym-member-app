84ce80b299f241fb9f0b54d79823a36c2c4af3e0
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-01-28 17:20:30 +0100

    Added a new item with text content in Dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,12 @@
                     You're logged in!
                 </div>
             </div>
+            <br>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Start managing the Gym Members!
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
